1f17098dace8ff1eee51dc8043c402c94cd37aaa
SeungjunLee <juny2400@cryptolab.co.kr>
2026-03-20 06:50:30 +0000

    fix ZO sampling bug

--- src/Encryptor.cpp
+++ src/Encryptor.cpp
@@ -353,13 +353,15 @@ template <Preset P> void EncryptorT<P>::sampleZO(Size num_polyunit) const {
     }
 
     PRAGMA_OMP(omp for schedule(static))
-    for (Size i = 0; i < degree; ++i) {
-        u64 &rnd = samples_[i / 32];
+    for (Size i = 0; i < degree - 1; ++i) {
+        u64 &rnd = samples_[degree - pad_num + i / 32];
         // mask is 0xFFFFFFFF if bit is 1, 0x0 if bit is 0
         mask_[i] = 0UL - ((rnd & 2) >> 1);
         samples_[i] = (rnd & 1);
         rnd >>= 2;
     }
+    mask_[degree - 1] = 0UL - ((samples_[degree - 1] & 2) >> 1);
+    samples_[degree - 1] &= 1;
 
     PRAGMA_OMP(omp for collapse(2) schedule(static))
     for (Size i = 0; i < num_polyunit; ++i) {
